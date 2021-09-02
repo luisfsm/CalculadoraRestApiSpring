8cb18c5cb24f6dac67879cf23ef616db4603949f
luis felipe <luis.s.mendes20@gmail.com>
2021-09-01 22:11:00 -0300

    ADD: escopo inicial

---
+++ src/main/java/br/com/calculadora/RestWithspringcalculadora/RestWithSpringCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.calculadora.RestWithspringcalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringCalculadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/calculadora/RestWithspringcalculadora/RestWithSpringCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calculadora.RestWithspringcalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
