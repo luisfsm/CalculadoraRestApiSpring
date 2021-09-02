e0ae7d30e782c06e0139ee53d329b7076083009d
luis felipe <luis.s.mendes20@gmail.com>
2021-09-01 22:42:26 -0300

    EDIT: ajustando controller da calculadora

--- src/main/java/br/com/calculadora/RestWithspringcalculadora/controller/ControllerCalculator.java
+++ src/main/java/br/com/calculadora/RestWithspringcalculadora/controller/ControllerCalculator.java
@@ -1,18 +1,17 @@
 package br.com.calculadora.RestWithspringcalculadora.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping
+@RestController
 public class ControllerCalculator {
 
-	@GetMapping("/sum/numberOne/numberTwo")
+	@RequestMapping("/sum/{numberOne}/{numberTwo}")
 	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo ) throws Exception {
 		Double sum;
 		if(isNumeric(numberOne) && isNumeric(numberTwo) ) {
+			System.out.println(numberOne);
+			System.out.println(numberTwo);
 			 sum = Double.parseDouble(numberOne)+Double.parseDouble(numberOne);
 		}else {
 			throw new Exception();
@@ -25,7 +24,7 @@ public class ControllerCalculator {
 	private boolean isNumeric(String strNumber) {
 		if(strNumber == null) return false;
 		String number = strNumber.replaceAll(",",".");
-		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 	
 }
